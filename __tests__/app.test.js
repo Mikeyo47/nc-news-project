8a48f378b3ab6e972406ae6f74b7e9bcc7e0ecfe
Mikeyo47 <kopecky_michal@yahoo.com>
2023-06-27 15:39:10 +0100

    complete task nc-news-3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,17 +3,18 @@ const seed = require("../db/seeds/seed");
 const request = require('supertest');
 const db = require('../db/connection');
 const data = require("../db/data/test-data");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
 
-describe("GET/api/topics", () => {
-    it("returns a status code of 200", () => {
+describe("GET /api/topics", () => {
+    it("responds with a status code of 200", () => {
         return request(app)
         .get("/api/topics")
         .expect(200);
     });
-    it("should have a 'slug' and 'description' key in each returned object within the topics array", () => {
+    it("should have a 'slug' and 'description' property in each returned object within the topics array", () => {
         return request(app)
         .get("/api/topics")
         .then(({body}) => {
@@ -24,9 +25,35 @@ describe("GET/api/topics", () => {
                         description: expect.any(String)
                     }));
                 })
-            } else {
-                expect(body.topics).toEqual([]);
             }
         });
     });
 });
+
+describe("GET /api", () => {
+    it("should respond with status code 200 and an object describing all the available endpoints", () => {
+        return request(app)
+        .get("/api")
+        .expect(200)
+        .then(({body}) => {
+             expect(body).toEqual(endpoints);
+        });
+    });
+    it("should check that the response object is of the correct format", () => {
+        return request(app)
+        .get("/api/")
+        .then(({body}) => {
+            if (Object.keys(body).length > 0) {
+                for (let api in body) {
+                    if (api !== "GET /api") {
+                        expect(body[api]).toEqual(expect.objectContaining({
+                            description: expect.any(String),
+                            queries: expect.any(Array),
+                            exampleResponse: expect.any(Object)
+                        }));
+                    }
+                }
+            }
+        });
+    });
+})
--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const {
     getTopics
 } = require("./controllers/topics.controllers");
+const endpoints = require("./endpoints.json");
 
 const app = express();
 
 app.get('/api/topics', getTopics);
+
+app.get('/api', (_, res) => {
+    res.status(200).send(endpoints);
+});
 
 module.exports = app;
